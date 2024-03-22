c5d20c2da121d0c2366d0f647b4db29c61a4c259
Rudraksh Naik <55753415+rud-rax@users.noreply.github.com>
2024-03-22 20:01:33 +0530

    Added more mocking tests

--- src/mocking/service.py
+++ src/mocking/service.py
@@ -2,8 +2,18 @@
 import sys
 sys.path.append(".")
 
+import requests
+
 from src.mocking.db import phonetic_letters
 
 def get_phonetic_from_db(id):
     return phonetic_letters[id]
 
+def get_users():
+    
+    response = requests.get(r"https://jsonplaceholder.typicode.com/users")
+    
+    if response.status_code == 200:
+        return response.json()
+    
+    raise requests.HTTPError
--- tests/test_mocking.py
+++ tests/test_mocking.py
@@ -5,6 +5,8 @@ sys.path.append(".")
 import pytest
 import unittest.mock as mock
 
+import requests
+
 from src.mocking import service as service
 
 
@@ -16,3 +18,69 @@ def test_get_phonetic_from_db(mock_get_phonetic_from_db):
     phonetic = service.get_phonetic_from_db(2)
     
     assert phonetic == "Mocked Bravo"
+    
+
+@mock.patch("requests.get")
+def test_get_users(mock_get):
+    
+    mock_response = mock.Mock()
+    mock_response.status_code = 200 
+    mock_response.json.return_value = {"id" : 8888 , "name" : "Bill"}
+    
+    mock_get.return_value = mock_response
+    
+    data = service.get_users()
+    
+    assert data == {"id" : 8888 , "name" : "Bill"}
+    
+    
+@mock.patch("requests.get")
+def test_get_users_error(mock_get):
+    
+    mock_response = mock.Mock()
+    mock_response.status_code = 400
+    
+    mock_get.return_value = mock_response
+    
+    with pytest.raises(requests.HTTPError) :
+        service.get_users()
+        
+        
+def test_spy_method(mocker):
+    class Foo(object):
+        def bar(self, v):
+            return v * 2
+
+    foo = Foo()
+    spy = mocker.spy(foo, 'bar')
+    assert foo.bar(21) == 42
+
+    spy.assert_called_once_with(21)
+    assert spy.spy_return == 42
+
+
+
+def test_spy_function(mocker):
+
+    spy = mocker.spy(service, "get_phonetic_from_db")
+    
+    
+    
+    assert service.get_phonetic_from_db(1) == "Alpha"
+    assert service.get_phonetic_from_db(2) == "Bravo"
+    assert service.get_phonetic_from_db(3) == "Charlie"
+    
+    assert spy.call_count == 3
+    assert spy.spy_return == "Charlie"
+    
+
+# def test_get_users_error2(mocker):
+    
+#     spy = mocker.spy(service , "requests.get")
+    
+#     mock_response = mock.Mock()
+#     mock_response.status_code = 400
+    
+    
+#     with pytest.raises(requests.HTTPError) :
+#         service.get_users()
